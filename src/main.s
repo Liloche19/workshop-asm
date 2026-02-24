d3f9a0b28ed3bb46eb5c47fe5d5208b42e8b3f69
Liloche19 <lilian.locher@epitech.eu>
2026-02-24 11:33:38 +0100

    feat(ex03): completed

--- src/main.s
+++ src/main.s
@@ -1,20 +1,34 @@
-EXIT equ 0x3C
 WRITE equ 0x1
+EXIT equ 0x3C
+
+section .data
+    msg db 'Hello, world!', 0xA, 0x0    ; set msg variable
 
 section .text
     global _start
 
 _start:
+    lea rdi, [msg]      ; Message to compute the length
+    call strlen         ; Calculate the length of the message
+
     mov rax, WRITE      ; Write system call
-    mov rdi, 1          ; File descriptor for stdout
     mov rsi, msg        ; Message to print
-    mov rdx, msg_len    ; Length of message
+    mov rdi, 1          ; File descriptor for stdout
     syscall             ; Invoke system call
 
     mov rax, EXIT       ; Exit system call
     mov rdi, 0          ; Exit code 0
     syscall             ; Invoke system call
 
-section .data
-msg db 'Hello, world!', 0xA         ; set msg variable
-msg_len equ $ - msg                 ; set the msg_len to the length of msg
+strlen:
+    mov rdx, 0          ; Initialize the counter to 0
+
+.loop:
+    cmp byte [rdi], 0   ; Verify if the current character is 0
+    je .end             ; If equal to 0, then jump to .end
+    inc rdx             ; Otherwise, increment the counter
+    inc rdi             ; Move to the next character
+    jmp .loop           ; Call the loop
+
+.end:
+    ret                 ; End of the function
