aa8fde177c44920c9392da1d4c80eb6ffe40c044
Liloche19 <lilian.locher@epitech.eu>
2026-02-24 10:45:39 +0100

    feat(ex02): completed

--- src/main.s
+++ src/main.s
@@ -1,7 +1,20 @@
+EXIT equ 0x3C
+WRITE equ 0x1
+
 section .text
     global _start
 
 _start:
-    mov rax, 0x3C   ; Exit system call
-    mov rdi, 0      ; Exit code 0
-    syscall         ; Invoke system call
+    mov rax, WRITE      ; Write system call
+    mov rdi, 1          ; File descriptor for stdout
+    mov rsi, msg        ; Message to print
+    mov rdx, msg_len    ; Length of message
+    syscall             ; Invoke system call
+
+    mov rax, EXIT       ; Exit system call
+    mov rdi, 0          ; Exit code 0
+    syscall             ; Invoke system call
+
+section .data
+msg db 'Hello, world!', 0xA         ; set msg variable
+msg_len equ $ - msg                 ; set the msg_len to the length of msg
