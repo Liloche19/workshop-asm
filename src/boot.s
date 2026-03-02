18b4cbbae2fbbbb7769133c549567eb02359d24d
Liloche19 <lilian.locher@epitech.eu>
2026-03-02 10:36:27 +0100

    feat(ex02): completed

--- src/boot.s
+++ src/boot.s
@@ -2,7 +2,27 @@
 org 0x7C00
 
 start:
+    xor ax, ax
+    mov ds, ax
+    mov es, ax
+
+    mov ah, 0x0E
+    mov bx, msg
+    call print_string
     jmp $
+
+print_string:
+    mov al, [bx]
+    cmp al, 0
+    je end_print
+    int 0x10
+    inc bx
+    jmp print_string
+
+end_print:
+    ret
+
+msg db 'Hello Boot', 0
 
 times 510-($-$$) db 0
 dw 0xAA55
